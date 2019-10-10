dc6abb0549441ebe42ec23d241b37c2d57d674a1
Poline Fedorova <polina.fedorova@devexpress.com>
2019-10-10 18:06:40 +0300

    add project files

---
+++ CS/DXRichEdit_TrackChanges/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using DevExpress.Xpf.Core;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace DXRichEdit_TrackChanges
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            richEditControl1.LoadDocument("DocumentWithRevisions.docx");
+            richEditControl1.AnnotationOptions.VisibleAuthors.Remove("Michael Suyama");
+
+            AcceptAndRejectRevisions();
+            richEditControl1.Document.SaveDocument("DocumentWithRevisions.docx", DocumentFormat.OpenXml);
+
+
+        }
+
+        private void AcceptAndRejectRevisions()
+        {
+            RevisionCollection documentRevisions = richEditControl1.Document.Revisions;
+
+            //Reject all revisions in the firts page's header:
+            SubDocument header = richEditControl1.Document.Sections[0].BeginUpdateHeader(HeaderFooterType.First);
+            documentRevisions.RejectAll(header);
+            richEditControl1.Document.Sections[0].EndUpdateHeader(header);
+
+            //Reject all revisions from the specific author on the first section:
+            var sectionRevisions = documentRevisions.Get(richEditControl1.Document.Sections[0].Range).Where(x => x.Author == "Janet Leverling");
+
+            foreach (Revision revision in sectionRevisions)
+                revision.Reject();
+
+            //Accept all format changes:
+            documentRevisions.AcceptAll(x => x.Type == RevisionType.CharacterPropertyChanged || x.Type == RevisionType.ParagraphPropertyChanged || x.Type == RevisionType.SectionPropertyChanged);
+        }
+
+        private void RichEditControl_TrackedMovesConflict(object sender, TrackedMovesConflictEventArgs e)
+        {
+            //Compare the length of the original and new location ranges
+            //Keep text from the location whose range is the smallest
+            e.ResolveMode = (e.OriginalLocationRange.Length <= e.NewLocationRange.Length) ? TrackedMovesConflictResolveMode.KeepOriginalLocationText : TrackedMovesConflictResolveMode.KeepNewLocationText;
+        }
+    }
+}
---
+++ VB/DXRichEdit_TrackChanges/MainWindow.xaml.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Linq
+Imports DevExpress.Xpf.Core
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace DXRichEdit_TrackChanges
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits ThemedWindow
+
+		Public Sub New()
+			InitializeComponent()
+
+			richEditControl1.LoadDocument("DocumentWithRevisions.docx")
+			richEditControl1.AnnotationOptions.VisibleAuthors.Remove("Michael Suyama")
+
+			AcceptAndRejectRevisions()
+			richEditControl1.Document.SaveDocument("DocumentWithRevisions.docx", DocumentFormat.OpenXml)
+
+
+		End Sub
+
+		Private Sub AcceptAndRejectRevisions()
+			Dim documentRevisions As RevisionCollection = richEditControl1.Document.Revisions
+
+			'Reject all revisions in the firts page's header:
+			Dim header As SubDocument = richEditControl1.Document.Sections(0).BeginUpdateHeader(HeaderFooterType.First)
+			documentRevisions.RejectAll(header)
+			richEditControl1.Document.Sections(0).EndUpdateHeader(header)
+
+			'Reject all revisions from the specific author on the first section:
+			Dim sectionRevisions = documentRevisions.Get(richEditControl1.Document.Sections(0).Range).Where(Function(x) x.Author = "Janet Leverling")
+
+			For Each revision As Revision In sectionRevisions
+				revision.Reject()
+			Next revision
+
+			'Accept all format changes:
+			documentRevisions.AcceptAll(Function(x) x.Type = RevisionType.CharacterPropertyChanged OrElse x.Type = RevisionType.ParagraphPropertyChanged OrElse x.Type = RevisionType.SectionPropertyChanged)
+		End Sub
+
+		Private Sub RichEditControl_TrackedMovesConflict(ByVal sender As Object, ByVal e As TrackedMovesConflictEventArgs)
+			'Compare the length of the original and new location ranges
+			'Keep text from the location whose range is the smallest
+			e.ResolveMode = If(e.OriginalLocationRange.Length <= e.NewLocationRange.Length, TrackedMovesConflictResolveMode.KeepOriginalLocationText, TrackedMovesConflictResolveMode.KeepNewLocationText)
+		End Sub
+	End Class
+End Namespace

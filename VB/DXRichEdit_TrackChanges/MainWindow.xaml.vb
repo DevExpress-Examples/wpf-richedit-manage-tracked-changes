287f5cbce1634711e25b456f42677fb20d09c082
DevExpressExampleBot <fakeEmail>
2023-09-25 20:01:50 +0400

    Source auto update [skip ci]

--- VB/DXRichEdit_TrackChanges/MainWindow.xaml.vb
+++ VB/DXRichEdit_TrackChanges/MainWindow.xaml.vb
@@ -1,50 +1,44 @@
-﻿Imports System.Linq
+Imports System.Linq
 Imports DevExpress.Xpf.Core
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace DXRichEdit_TrackChanges
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits ThemedWindow
 
-		Public Sub New()
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits ThemedWindow
 
-			richEditControl1.LoadDocument("DocumentWithRevisions.docx")
-			richEditControl1.AnnotationOptions.VisibleAuthors.Remove("Michael Suyama")
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.richEditControl1.LoadDocument("DocumentWithRevisions.docx")
+            Me.richEditControl1.AnnotationOptions.VisibleAuthors.Remove("Michael Suyama")
+            AcceptAndRejectRevisions()
+            Me.richEditControl1.Document.SaveDocument("DocumentWithRevisions.docx", DocumentFormat.OpenXml)
+        End Sub
 
-			AcceptAndRejectRevisions()
-			richEditControl1.Document.SaveDocument("DocumentWithRevisions.docx", DocumentFormat.OpenXml)
+        Private Sub AcceptAndRejectRevisions()
+            Dim documentRevisions As RevisionCollection = Me.richEditControl1.Document.Revisions
+            'Reject all revisions in the firts page's header:
+            Dim header As SubDocument = Me.richEditControl1.Document.Sections(0).BeginUpdateHeader(HeaderFooterType.First)
+            documentRevisions.RejectAll(header)
+            Me.richEditControl1.Document.Sections(0).EndUpdateHeader(header)
+            'Reject all revisions from the specific author on the first section:
+            Dim sectionRevisions = documentRevisions.Get(Me.richEditControl1.Document.Sections(CInt(0)).Range).Where(Function(x) Equals(x.Author, "Janet Leverling"))
+            For Each revision As Revision In sectionRevisions
+                revision.Reject()
+            Next
 
+            'Accept all format changes:
+            documentRevisions.AcceptAll(Function(x) x.Type = RevisionType.CharacterPropertyChanged OrElse x.Type = RevisionType.ParagraphPropertyChanged OrElse x.Type = RevisionType.SectionPropertyChanged)
+        End Sub
 
-		End Sub
-
-		Private Sub AcceptAndRejectRevisions()
-			Dim documentRevisions As RevisionCollection = richEditControl1.Document.Revisions
-
-			'Reject all revisions in the firts page's header:
-			Dim header As SubDocument = richEditControl1.Document.Sections(0).BeginUpdateHeader(HeaderFooterType.First)
-			documentRevisions.RejectAll(header)
-			richEditControl1.Document.Sections(0).EndUpdateHeader(header)
-
-			'Reject all revisions from the specific author on the first section:
-			Dim sectionRevisions = documentRevisions.Get(richEditControl1.Document.Sections(0).Range).Where(Function(x) x.Author = "Janet Leverling")
-
-			For Each revision As Revision In sectionRevisions
-				revision.Reject()
-			Next revision
-
-			'Accept all format changes:
-			documentRevisions.AcceptAll(Function(x) x.Type = RevisionType.CharacterPropertyChanged OrElse x.Type = RevisionType.ParagraphPropertyChanged OrElse x.Type = RevisionType.SectionPropertyChanged)
-		End Sub
-
-		Private Sub RichEditControl_TrackedMovesConflict(ByVal sender As Object, ByVal e As TrackedMovesConflictEventArgs)
-			'Compare the length of the original and new location ranges
-			'Keep text from the location whose range is the smallest
-			e.ResolveMode = If(e.OriginalLocationRange.Length <= e.NewLocationRange.Length, TrackedMovesConflictResolveMode.KeepOriginalLocationText, TrackedMovesConflictResolveMode.KeepNewLocationText)
-		End Sub
-	End Class
+        Private Sub RichEditControl_TrackedMovesConflict(ByVal sender As Object, ByVal e As TrackedMovesConflictEventArgs)
+            'Compare the length of the original and new location ranges
+            'Keep text from the location whose range is the smallest
+            e.ResolveMode = If(e.OriginalLocationRange.Length <= e.NewLocationRange.Length, TrackedMovesConflictResolveMode.KeepOriginalLocationText, TrackedMovesConflictResolveMode.KeepNewLocationText)
+        End Sub
+    End Class
 End Namespace
